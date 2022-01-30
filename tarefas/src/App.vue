6ddcf0953c69ecad9131cb7d9ecc352632c624c7
Lauro Coelho Chicorski <laurocoelhoc@gmail.com>
2022-01-30 09:32:27 -0300

    Sessao 10: Componente Nova Tarefa #01

--- tarefas/src/App.vue
+++ tarefas/src/App.vue
@@ -1,21 +1,33 @@
 <template>
 	<div id="app">
 		<h1>Tarefas</h1>
+		<NewTask @taskAdded="addTask"></NewTask>
 		<TaskGrid :tasks="tasks"></TaskGrid>
 	</div>
 </template>
 
 <script>
 import TaskGrid from './components/TaskGrid.vue'
+import NewTask from './components/NewTask.vue'
 
 export default {
-	components: {TaskGrid},
+	components: {TaskGrid, NewTask},
 	data() {
 		return {
 			tasks: [
 				{ name: 'Lavar a louça', pending: false},
 				{ name: 'Comprar blusa', pending: true}
 			]
+		}
+	},
+	methods: {
+		addTask(task) {
+			const sameName = t => t.name === task.name
+			const reallyNew = this.tasks.filter(sameName).length == 0
+			reallyNew && this.tasks.push({
+				name: task.name,
+				pending: task.pending || true
+			})
 		}
 	}
 }
---
+++ tarefas/src/components/NewTask.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="new-tasks">
+      <input v-model="name" @keydowm.enter="add" type="text">
+      <button @click="add">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name: ''
+        }
+    },
+    methods: {
+        add() {
+            this.$emit('taskAdded', {name: this.name})
+            this.name = ''
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
