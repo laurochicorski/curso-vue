c1cf832eba6e8371bbce07a636d99842aa2d13a9
Lauro Coelho Chicorski <laurocoelhoc@gmail.com>
2022-02-05 17:13:18 -0300

    Modificando Diretivas Personalizadas com Modifificadores

--- diretivas-exercicios/src/App.vue
+++ diretivas-exercicios/src/App.vue
@@ -5,7 +5,7 @@
 		<p v-text="'Usando diretiva v-text'"></p>
 		<p v-html="'Usando diretiva <strong>v-html</strong>'"></p>
 		<hr>
-		<p v-destaque:fundo="'lightblue'">Usando Diretiva Personalizada</p>
+		<p v-destaque:fundo.atrasar="'lightblue'">Usando Diretiva Personalizada</p>
 		<p v-destaque="cor">Usando Diretiva Personalizada</p>
 	</div>
 </template>
