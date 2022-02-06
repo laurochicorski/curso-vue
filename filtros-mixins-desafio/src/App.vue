cc8041557fedddc7cdb074ed89cbc81fe1a8e01a
Lauro Coelho Chicorski <laurocoelhoc@gmail.com>
2022-02-05 22:59:42 -0300

    Hora de Praticar - Filtros & Mixins (Resposta)

--- filtros-mixins-desafio/src/App.vue
+++ filtros-mixins-desafio/src/App.vue
@@ -3,14 +3,24 @@
 		<h1>Filtros & Mixins (Desafio)</h1>
 		<!-- Exercício 1 -->
 		<!-- Construir um filtro local que troca espaços por vírgula -->
+		<p>{{ frase }}</p>
+		<span>{{ frase | trocaEspacoPorVirgula }}</span>
 		
 		<!-- Exercício 2 -->
 		<!-- Filtro global que conta o tamanho de cada palavra e adiciona o 
 			valor na string final -->
 		<!-- "Pedro é legal" => "Pedro (5) é (1) legal (5)" -->
+		<hr>
+		<span>{{ frase2 | contaQuantidaLetras}}</span>
+		<hr>
+		<span>{{ frase2 | contaQuantida-letras-2}}</span>
 
 		<!-- Exercício 3 -->
 		<!-- Implementar os exercicios 1 e 2 com propriedade computada -->
+		<hr>
+		<span>{{ frase1Pronta }}</span>
+		<hr>
+		<span>{{ frase2Pronta }}</span>
 
 		<!-- Exercício 4 -->
 		<!-- Compartilhe a propriedade computada via mixin -->
@@ -19,7 +29,34 @@
 
 <script>
 export default {
-	
+	filters: {
+		trocaEspacoPorVirgula(valor) {
+			return valor.replaceAll(' ', ',')
+		}
+
+	},
+	data() {
+		return {
+			frase: 'carro avião trem',
+			frase2: 'Pedro é legal'
+		}
+	},
+	computed: {
+		frase1Pronta() {
+			return this.frase.replaceAll(' ', ',')
+		},
+		frase2Pronta() {
+			let arr = this.frase2.split(' ')
+
+			let arrResposta = [];
+
+			arr.forEach(function(palavra) {
+				arrResposta.push(palavra + ' (' + palavra.length + ') ')
+			})
+			
+			return arrResposta.join()
+		}
+	}
 }
 </script>
 
--- filtros-mixins-desafio/src/main.js
+++ filtros-mixins-desafio/src/main.js
@@ -3,6 +3,23 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.filter('contaQuantidaLetras', function(valor){
+	let arr = valor.split(' ')
+
+	let arrResposta = [];
+
+	arr.forEach(function(palavra) {
+		arrResposta.push(palavra + ' (' + palavra.length + ') ')
+	})
+	
+	return arrResposta.join()
+})
+
+Vue.filter('contaQuantidaLetras2', function(valor){
+	return valor.split(' ').map(palavra => `${palavra} (${palavra.length}) `).join(' ')
+})
+
+
 new Vue({
 	render: h => h(App),
 }).$mount('#app')
