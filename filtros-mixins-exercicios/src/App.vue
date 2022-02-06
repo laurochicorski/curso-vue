acdf4d49367a694d50a36017b2f4f06aae20373b
Lauro Coelho Chicorski <laurocoelhoc@gmail.com>
2022-02-05 21:44:45 -0300

    Criando um filtro local

--- filtros-mixins-exercicios/src/App.vue
+++ filtros-mixins-exercicios/src/App.vue
@@ -2,12 +2,21 @@
 	<div id="app">
 		<h1>Filtros & Mixins</h1>
 		<hr>
-		<p>{{ cpf }}</p>
+		<p>{{ cpf | cpf}}</p>
 	</div>
 </template>
 
 <script>
 export default {
+	filters: {
+		cpf(valor) {
+			const arr = valor.split('')
+			arr.splice(3, 0, '.')
+			arr.splice(7, 0, '.')
+			arr.splice(11, 0, '-')
+			return arr.join('')
+		}
+	},
 	data() {
 		return {
 			cpf: '60070080090'
