dd201e54a552b8720afeb210caae76676228ce17
Lauro Coelho Chicorski <laurocoelhoc@gmail.com>
2022-02-09 21:06:20 -0300

    Usando Nomes e Atributos Dinamicos

--- animacoes-exercicios/src/App.vue
+++ animacoes-exercicios/src/App.vue
@@ -4,7 +4,7 @@
 		<hr>
 		<b-button variant="primary" class="mb-4"
 			@click="exibir = !exibir">Mostar Mensagem</b-button>
-		<transition name="fade" appear>
+		<!-- <transition name="fade" appear>
 			<b-alert variant="info" show v-if="exibir">{{ msg }}</b-alert>	
 		</transition>
 		<transition name="slide" type="animation" appear>
@@ -13,6 +13,14 @@
 		<transition 
 			enter-active-class="animated bounce"
 			leave-active-class="animated shake">
+			<b-alert variant="info" show v-show="exibir">{{ msg }}</b-alert>	
+		</transition> -->
+		<hr>
+		<b-select v-model="tipoAnimacao" class="mb-4">
+			<option value="fade">Fade</option>
+			<option value="slide">Slide</option>
+		</b-select>
+		<transition :name="tipoAnimacao">
 			<b-alert variant="info" show v-show="exibir">{{ msg }}</b-alert>	
 		</transition>
 	</div>
@@ -24,7 +32,8 @@ export default {
 	data() {
 		return {
 			msg: 'Uma mensagem de informação para o usuário!',
-			exibir: false
+			exibir: false,
+			tipoAnimacao: 'fade'
 		}
 	}
 }
