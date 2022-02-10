a504d39bbad53e0bdcd24722a943726bf101eb13
Lauro Coelho Chicorski <laurocoelhoc@gmail.com>
2022-02-09 23:19:43 -0300

    DEsenvolvendo a Aplicacao #02

--- super-quiz/src/App.vue
+++ super-quiz/src/App.vue
@@ -1,9 +1,13 @@
 <template>
 	<div id="app">
 		<h1>Super Quiz</h1>
-		<Question v-if="questionMode" :question="questions[currentQuestion]">
+		<Question v-if="questionMode" 
+			:question="questions[currentQuestion]"
+			@answered="showResult">
 		</Question>
-		<Result v-else :result="result"/>
+		<Result v-else 
+			:result="result"
+			@confirmed="nextQuestion"/>
 	</div>
 </template>
 
@@ -20,6 +24,17 @@ export default {
 			questionMode: true,
 			questions,
 			currentQuestion: 0
+		}
+	},
+	methods: {
+		showResult(result) {
+			this.result = result
+			this.questionMode = false
+		},
+		nextQuestion() {
+			let r = Math.random() * this.questions.length
+			this.currentQuestion = parseInt(r)
+			this.questionMode = true	
 		}
 	}
 }
