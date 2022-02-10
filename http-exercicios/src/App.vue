7ca163e89cd4c43d1fdd00728e1edcaed4f33b74
Lauro Coelho Chicorski <laurocoelhoc@gmail.com>
2022-02-09 23:58:03 -0300

    Enviando POST

--- http-exercicios/src/App.vue
+++ http-exercicios/src/App.vue
@@ -1,17 +1,50 @@
 <template>
 	<div id="app" class="container">
 		<h1>HTTP com Axios</h1>
+		<b-card>
+			<b-form-group label="Nome:">
+				<b-form-input type="text" size="lg"
+					v-model="usuario.nome"
+					placeholder="Informe o Nome">
+				</b-form-input>
+			</b-form-group>
+			<b-form-group label="E-mail:">
+				<b-form-input type="email" size="lg"
+					v-model="usuario.email"
+					placeholder="Informe o E-mail">
+				</b-form-input>
+			</b-form-group>
+			<hr>
+			<b-button @click="salvar" size="lg" variant="primary">Salvar</b-button>
+		</b-card>
 	</div>
 </template>
 
 <script>
 export default {
-	created() {
-		this.$http.post('usuarios.json', {
-			nome: 'Maria',
-			email: 'maria_maria@gmail.com'
-		}).then(res => console.log(res))
+	data() {
+		return {
+			usuario: {
+				nome: '',
+				email: ''
+			}
+		}
+	},
+	methods: {
+		salvar() {
+			this.$http.post('usuarios.json', this.usuario)
+				.then(resp => {
+					this.usuario.nome = ''
+					this.usuario.email = ''
+				})
+		}
 	}
+	// created() {
+	// 	this.$http.post('usuarios.json', {
+	// 		nome: 'Maria',
+	// 		email: 'maria_maria@gmail.com'
+	// 	}).then(res => console.log(res))
+	// }
 }
 </script>
 
