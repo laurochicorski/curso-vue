b93e26fc8d0dd94960e83fbbeb9efb7140f027a9
Lauro Coelho Chicorski <laurocoelhoc@gmail.com>
2022-02-13 10:46:51 -0300

    Configurando Rotas Filhas (Rotas Aninhadas)

---
+++ rotas-exercicios/src/components/usuario/UsuarioDetalhe.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="usuario-detalhe">
+      <h3>Usuário Detalhe</h3>
+      <hr>
+      <p><strong>Código:</strong> {{ id }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['id']
+}
+</script>
+
+<style>
+
+</style>
---
+++ rotas-exercicios/src/components/usuario/UsuarioEditar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="usuario-id">
+      <h3>Usuário Editar</h3>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
