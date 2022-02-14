181fae6b359ab3400a7db35307cc6824481bb345
Lauro Coelho Chicorski <laurocoelhoc@gmail.com>
2022-02-14 08:07:21 -0300

    Tornando Router Links mais Dinamico

--- rotas-exercicios/src/components/usuario/UsuarioEditar.vue
+++ rotas-exercicios/src/components/usuario/UsuarioEditar.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="usuario-id">
       <h3>Usuário Editar</h3>
+      <p><strong>Código:</strong> {{ id }} / {{ $route.params.id }} </p>
   </div>
 </template>
 
 <script>
 export default {
-
+  props: ['id']
 }
 </script>
 
