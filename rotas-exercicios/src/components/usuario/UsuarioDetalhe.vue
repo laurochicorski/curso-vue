feb9b674b73b9b1bd1aaea24d7d6653c40be85cc
Lauro Coelho Chicorski <laurocoelhoc@gmail.com>
2022-02-14 08:44:54 -0300

    Passando Fragmento Hash

--- rotas-exercicios/src/components/usuario/UsuarioDetalhe.vue
+++ rotas-exercicios/src/components/usuario/UsuarioDetalhe.vue
@@ -5,7 +5,8 @@
       <p><strong>Código:</strong> {{ id }}</p>
       <router-link tag="button" primario
         :to="{ name: 'editarUsuario', params: { id },
-          query: { completo: true, lingua: 'pt' }}">
+          query: { completo: true, lingua: 'pt' },
+          hash: '#rodape' }">
         Editar
       </router-link>
   </div>
